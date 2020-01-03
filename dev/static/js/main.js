b728d74e751ecb275fc7d2811607d9f0f1d527ca
Ibragim <utisey@gmial.com>
2020-01-03 14:03:22 +0300

    some changes

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -26,70 +26,67 @@ $(document).ready(function () {
 
 
 
+// ----------------------------------------------- blog-tabs----start
+
+    let postsTab;
+    let postsContent;
+
+    window.onload=function(){
+        postsContent = document.getElementsByClassName('posts__content');
+        postsTab = document.getElementsByClassName('posts__tab');
+        hideTabsContent(1);
+    }
+
+    function hideTabsContent(a) {
+        for (let i=a; i<postsContent.length; i++) {
+            postsContent[i].classList.remove('show');
+            postsContent[i].classList.add('hide');
+            postsTab[i].classList.remove('active-border');
+        }
+    }
+
+    document.getElementById('posts__tabs').onclick = function(event) {
+        let target=event.target;
+        if (target.className=='posts__tab') {
+            for (let i=0; i<postsTab.length; i++) {
+                if(target == postsTab[i]) {
+                    showTabsContent(i);
+                    break;
+                }
+            }
+        }
+    }
+
+    function showTabsContent(b) {
+        if (postsContent[b].classList.contains('hide')) {
+            hideTabsContent(0);
+            postsTab[b].classList.add('active-border');
+            postsContent[b].classList.remove('hide');
+            postsContent[b].classList.add('show');
+        }
+    }
+
+// ----------------------------------------------- blog-tabs----end
 
 });
+
+
 
 $('.menu').on('click', function (e) {
     e.preventDefault();
     $('.menu__link').toggleClass('menu-active')
     $('.header-nav__list').toggleClass('menu-active')
-})
+});
 
 
 (function($) {
     $(function() {
-    
-    $('ul.tabs__caption').on('click', 'li.tabs__item:not(.active)', function() {
 
+    $('ul.tabs__caption').on('click', 'li.tabs__item:not(.active)', function() {
         $(this)
         .addClass('active').siblings().removeClass('active')
         .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
     });
-    
+
     });
 })(jQuery);
-
-
-// ----------------------------------------------- blog-tabs----start
-
-
-let postsTab;
-let postsContent;
-
-window.onload=function(){
-    postsContent = document.getElementsByClassName('posts__content');
-    postsTab = document.getElementsByClassName('posts__tab');
-    hideTabsContent(1);
-}
-
-function hideTabsContent(a) {
-    for (let i=a; i<postsContent.length; i++) {
-        postsContent[i].classList.remove('show');
-        postsContent[i].classList.add('hide');
-        postsTab[i].classList.remove('active-border');
-    }
-}
-
-document.getElementById('posts__tabs').onclick = function(event) {
-    let target=event.target;
-    if (target.className=='posts__tab') {
-        for (let i=0; i<postsTab.length; i++) {
-            if(target == postsTab[i]) {
-                showTabsContent(i);
-                break;
-            }
-        }
-    }
-}
-
-function showTabsContent(b) {
-    if (postsContent[b].classList.contains('hide')) {
-        hideTabsContent(0);
-        postsTab[b].classList.add('active-border');
-        postsContent[b].classList.remove('hide');
-        postsContent[b].classList.add('show');
-    }
-}
-
-
-// ----------------------------------------------- blog-tabs----end
